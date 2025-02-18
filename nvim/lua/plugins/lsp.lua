4de3f677ad728879f54a200c63cd44ce2bdf6955
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-02-18 13:21:41 +0000

    style(nvim): adjust indentation for consistency

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,66 +1,66 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"j-hui/fidget.nvim",
-	},
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "j-hui/fidget.nvim",
+  },
 
-	config = function()
-		local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities())
+  config = function()
+    local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities())
 
-		require("fidget").setup({})
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"html",
-				"cssls",
-				"clangd",
-				"omnisharp",
-				"bashls",
-				"rust_analyzer",
-				"pyright",
-				"ts_ls",
-				"gopls",
-			},
+    require("fidget").setup({})
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "html",
+        "cssls",
+        "clangd",
+        "omnisharp",
+        "bashls",
+        "rust_analyzer",
+        "pyright",
+        "ts_ls",
+        "gopls",
+      },
 
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
 
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim", "it", "describe", "before_each", "after_each" },
-								},
-							},
-						},
-					})
-				end,
-				["gopls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.gopls.setup({
-						filetypes = { "go", "gomod", "gowork", "gotmpl" },
-						settings = {
-							env = {
-								GOEXPERIMENT = "rangefunc",
-							},
-							formatting = {
-								gofumpt = true,
-							},
-						},
-					})
-				end,
-			},
-		})
-	end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim", "it", "describe", "before_each", "after_each" },
+                },
+              },
+            },
+          })
+        end,
+        ["gopls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.gopls.setup({
+            filetypes = { "go", "gomod", "gowork", "gotmpl" },
+            settings = {
+              env = {
+                GOEXPERIMENT = "rangefunc",
+              },
+              formatting = {
+                gofumpt = true,
+              },
+            },
+          })
+        end,
+      },
+    })
+  end,
 }
