c0dfde72ce7a55af8df5a21fc14445088daf0da2
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-03-19 14:36:21 +0000

    style(nvim): Modify indentation in autocmds.lua

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -2,11 +2,11 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight text on yank
 autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 40,
-		})
-	end,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
 })
