bf2145bb6641e8e76cba26eab01df8bca9c1b759
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-06-30 18:05:54 +0100

    fix: fix git root detection and enable current_line_blame option

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,8 +1,11 @@
 return {
     "lewis6991/gitsigns.nvim",
-    -- event = { "BufReadPre", "BufNewFile" },
     event = "VeryLazy",
+    opts = {
+        current_line_blame = true,
+    },
     cond = function()
-        return vim.fn.isdirectory(vim.fn.finddir(".git", ".;")) ~= 0
+        local f = vim.api.nvim_buf_get_name(0)
+        return f ~= "" and vim.fn.isdirectory(vim.fn.finddir(".git", vim.fn.fnamemodify(f, ":p:h") .. ";")) ~= 0
     end,
 }
