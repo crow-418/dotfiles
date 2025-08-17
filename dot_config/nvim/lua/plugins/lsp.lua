1243fee5ca3e7effed86bbe9fa7afbcfc310072b
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-08-17 17:52:34 +0100

    feat: add mason to simplify installation of lsps, formatteres etc.
    use v0.11 vim.lsp.enable and vim.lsp.config functions as that's what's
    recommended officially
    
    create a Tools table to hold lsp configs and formatters list
    
    use mason-tools-installer to install the tools and mason-lspconfig as a
    dependency to be able to use the names of lsps in Tools table

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,41 +1,66 @@
-return {
-    {
-        "j-hui/fidget.nvim",
-        event = "LspAttach",
-        opts = {
-            notification = {
-                window = {
-                    winblend = 0,
-                }
-            }
-        },
+Tools = {
+    servers = {
+        lua_ls = {},
+        html = {},
+        cssls = {},
+        clangd = {},
+        omnisharp = {},
+        bashls = {},
+        rust_analyzer = {},
+        pyright = {},
+        ts_ls = {},
+        gopls = {},
     },
+    formatters = {
+        "goimports",
+        "prettierd",
+        "stylua",
+        "black",
+        "shfmt",
+        "yamlfmt",
+        "clang-format",
+        "csharpier",
+    },
+}
+
+return {
+    { "j-hui/fidget.nvim", event = "LspAttach", opts = {} },
     {
         "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            servers = {
-                lua_ls = {},
-                html = {},
-                cssls = {},
-                clangd = {},
-                omnisharp = {},
-                bashls = {},
-                rust_analyzer = {},
-                pyright = {},
-                ts_ls = {},
-                gopls = {},
-            },
-        },
-        config = function(_, opts)
-            local lspconfig = require("lspconfig")
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-            for server, server_opts in pairs(opts.servers) do
-                server_opts.capabilities =
-                    vim.tbl_deep_extend("force", {}, capabilities, server_opts.capabilities or {})
-                lspconfig[server].setup(server_opts)
+        event = { "BufReadPost", "BufReadPre", "BufNewFile" },
+        dependencies = { "mason-org/mason.nvim" },
+        config = function()
+            -- v0.11 and after use vim.lsp.enable("...") instead of
+            -- require'lspconfig'.….setup{}
+            for server_name, server_opts in pairs(Tools.servers) do
+                vim.lsp.config[server_name] = server_opts or {}
+                vim.lsp.enable(server_name)
             end
+        end,
+    },
+    {
+        "mason-org/mason.nvim",
+        cmd = { "Mason", "MasonInstall" },
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        build = ":MasonUpdate",
+        opts = {},
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = {
+            "mason-org/mason-lspconfig.nvim",
+        },
+        cmd = {
+            "MasonToolsInstall",
+            "MasonToolsInstallSync",
+            "MasonToolsUpdate",
+            "MasonToolsUpdateSync",
+            "MasonToolsClean",
+        },
+        config = function()
+            local ensure_installed = vim.tbl_keys(Tools.servers or {})
+            vim.list_extend(ensure_installed, Tools.formatters)
+            require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
         end,
     },
 }
