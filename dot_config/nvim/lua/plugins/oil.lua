075e360ffd5e6f2f8bec15c3a7507841d764070d
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-08-17 17:36:33 +0100

    feat: only have useful keybinds and disable the default ones for oil

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -5,10 +5,18 @@ return {
             { "-", "<cmd>Oil<cr>", desc = "Open Oil" },
         },
         opts = {
+            use_default_keymaps = false,
             default_file_explorer = true,
             skip_confirm_for_simple_edits = true,
             view_options = {
                 show_hidden = true,
+            },
+            keymaps = {
+                ["g?"] = { "actions.show_help", mode = "n" },
+                ["<CR>"] = "actions.select",
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
             },
         },
     },
