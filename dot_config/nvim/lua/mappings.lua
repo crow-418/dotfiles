413edd218e93b7774079d99fc73f8f4c7002acbb
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-08-17 17:01:32 +0100

    fix: removed redundant space + e keybind to vim diagnostics
    found out I can just use ctrl + w + d

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,8 +1,5 @@
 local map = vim.keymap.set
 
--- Display Lsp Diagnostics
-map("n", "<space>e", vim.diagnostic.open_float)
-
 -- Shift + K/J for moving lines up and down in visual mode
 map("x", "K", ":move '<-2<CR>gv=gv")
 map("x", "J", ":move '>+1<CR>gv=gv")
