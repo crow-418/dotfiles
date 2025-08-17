ea8822607f37d48918da82bfcf8c298cc0fe5a52
Iulian Nichifor <73722884+crow-418@users.noreply.github.com>
2025-08-17 17:45:08 +0100

    refactor: refactor implementation of colorscheme saving functionality
    remove "Themes" user command as there's no point in having it
    Telescope will handle it directly
    
    just save colorscheme Telescope change of it and on :colorscheme command
    
    remove usercmd alias as it's not used

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,5 +1,4 @@
 local autocmd = vim.api.nvim_create_autocmd
-local usercmd = vim.api.nvim_create_user_command
 
 autocmd("TextYankPost", {
     callback = function()
@@ -26,21 +25,6 @@ autocmd("BufReadPost", {
     desc = "go to last loc when opening a buffer",
 })
 
-autocmd("FileType", {
-    pattern = {
-        "grug-far",
-        "help",
-        "man",
-        "qf",
-        "query",
-    },
-    callback = function(event)
-        vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    end,
-    desc = "close certain windows with q",
-})
-
 -- Load the last used colorscheme from file, fallback to default
 local colorscheme_file = vim.fn.stdpath("data") .. "/colorscheme.txt"
 local current_theme = (vim.fn.filereadable(colorscheme_file) == 1 and vim.fn.readfile(colorscheme_file)[1]) or nil
@@ -49,28 +33,23 @@ current_theme = current_theme or "gruber-darker"
 
 vim.cmd.colorscheme(current_theme)
 
--- defer everything
-vim.defer_fn(function()
-    -- :Themes command
-    usercmd("Themes", function()
-        require("telescope.builtin").colorscheme({
-            enable_preview = true,
-        })
-    end, {})
-
-    -- write chosen colorscheme
-    autocmd("ColorScheme", {
-        callback = function(args)
-            local name = args.match
+autocmd("ColorScheme", {
+    callback = function(args)
+        local name = args.match
+        if name and #name > 0 then
             vim.fn.writefile({ name }, colorscheme_file)
-        end,
-    })
+        end
+    end,
+})
 
-    -- add colorschemes into rtp
+-- add colorschemes into rtp
+vim.defer_fn(function()
     local fs = vim.uv or vim.loop
+
     local plugins_ok, plugins = pcall(function()
         return require("lazy.core.config").plugins
     end)
+
     if plugins_ok then
         for _, plugin in pairs(plugins) do
             local colors_path = plugin.dir .. "/colors"
